d1f1be544697c1de059a87035daa47bc365f5df8
salih yazar <yzr.salih@gmail.com>
2025-08-16 11:37:57 +0200

    feat: add bootstrap base css import

--- myPersonalPortfolio/src/main.jsx
+++ myPersonalPortfolio/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+// Bootstrap temel stilleri
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
